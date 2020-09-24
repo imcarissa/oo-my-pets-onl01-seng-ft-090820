0f9e00e49a6ef484211bc807427f4f86feb7142c
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-24 19:13:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,12 +5,16 @@ class Dog
    attr_reader :name, :owner, :mood
    attr_writer :owner, :mood
    
+   @@all = []
+   
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   
   def self.all
+    @@all
   end
 end
