badb758fcecbb415d1e780628063b120fddf6b35
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-24 19:12:53 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,11 +3,13 @@ class Cat
   attr_reader :name, :owner, :mood
   attr_writer :owner, :mood
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
-    @@all = << self
+    @@all << self
   end
   
   def self.all 
